6cc5ad6bffe82a5975e65f8f58a50297ea776b29
Rahan <wangleibniz01@gmail.com>
2021-02-22 18:52:17 +0800

    Updates

---
+++ precache-manifest.3f240831bb8d89bec7008ed744fc6eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce69668f247a0ea613c",
+    "url": "/cardInfo/static/css/main.5b6c66e3.chunk.css"
+  },
+  {
+    "revision": "8ce69668f247a0ea613c",
+    "url": "/cardInfo/static/js/main.8ce69668.chunk.js"
+  },
+  {
+    "revision": "8849a8cdac0cdea91664",
+    "url": "/cardInfo/static/js/1.8849a8cd.chunk.js"
+  },
+  {
+    "revision": "1bdd6d6b7626cf7bc1ff",
+    "url": "/cardInfo/static/js/runtime~main.1bdd6d6b.js"
+  },
+  {
+    "revision": "d9b7c5bdf6ca0cbf327fb199613c8a3c",
+    "url": "/cardInfo/index.html"
+  }
+];
